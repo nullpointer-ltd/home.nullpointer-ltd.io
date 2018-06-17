04f063881babe8d1a5e1c058d241e7105b2deb42
duanqz <duanqz@gmail.com>
2018-06-17 16:49:31 +0800

    Update ui

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
 
   $(".menu-bar").on('click', function(e){
       e.preventDefault();
-      // $("nav").toggleClass('hide');
+      $("nav").toggleClass('hide');
       $("span", this).toggleClass("lnr-menu lnr-cross");
       $(".main-menu").addClass('mobile-menu');
   });
